d5ed5b7ef3a5fbe2d704c82f375e269e0e9ae17e
ByteMe6 <120343461+ByteMe6@users.noreply.github.com>
2024-06-02 12:36:06 +0000

    Deploying to gh-pages from @ ByteMe6/popular-games@e64c80e32d90cb7b5d88e84c3caaf964d5714999 🚀

--- index.64ee8586.js
+++
@@ -1,2 +0,0 @@
-var prevBtn=document.querySelector(".sliderBtnPrev"),nextBtn=document.querySelector(".sliderBtnNext"),sliderImg=document.querySelector(".sliderImg");prevBtn.addEventListener("click",(function(){}));
-//# sourceMappingURL=index.64ee8586.js.map
