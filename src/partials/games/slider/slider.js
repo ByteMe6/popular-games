059fd059118670f4d01d04e056bc3ea47576a7a8
Yurka2012 <tsapiy.yurka@gmail.com>
2024-06-02 14:44:26 +0300

    slider
     - продовження роботи з слайдером (ще не кінець, потім дороблю)

---
+++ src/partials/games/slider/slider.js
@@ -0,0 +1,6 @@
+const prevBtn = document.querySelector('.sliderBtnPrev');
+const nextBtn = document.querySelector('.sliderBtnNext');
+const sliderImg = document.querySelector('.sliderImg');
+prevBtn.addEventListener('click', () => {
+    
+})
