e64c80e32d90cb7b5d88e84c3caaf964d5714999
Yurka2012 <tsapiy.yurka@gmail.com>
2024-06-02 15:35:42 +0300

    11

--- src/partials/games/slider/slider.js
+++ src/partials/games/slider/slider.js
@@ -1,6 +1,20 @@
 const prevBtn = document.querySelector('.sliderBtnPrev');
 const nextBtn = document.querySelector('.sliderBtnNext');
-const sliderImg = document.querySelector('.sliderImg');
+const sliderImg = document.querySelectorAll('.sliderImg');
+let masterSlide = 0;
+const sliderImgWidth = document.querySelector('.slide').offsetWidth;
+const sliderImgBox = document.querySelector(".sliderImgBox");
+
+console.log("width:" + sliderImgWidth + "px");
+
 prevBtn.addEventListener('click', () => {
-    
-})
+    masterSlide = (masterSlide === 0) ? sliderImg.length - 1 : masterSlide - 1;
+    sliderImgBox.style.transform = `translateX(-${masterSlide * sliderImgWidth}px)`;
+    console.log(masterSlide);
+});
+
+nextBtn.addEventListener('click', () => {
+    masterSlide = (masterSlide === sliderImg.length - 1) ? 0 : masterSlide + 1;
+    sliderImgBox.style.transform = `translateX(-${masterSlide * sliderImgWidth}px)`;
+    console.log(masterSlide);
+});
